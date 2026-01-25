6025459617632b37d06bfd4362573e5811ddd036
Woarang <woarang00@gmail.com>
2026-01-25 13:54:53 -0500

    Commit: Update content.jsx with detailed work experience and skills; add intellij.png asset

--- my-react-app/src/components/content.jsx
+++ my-react-app/src/components/content.jsx
@@ -9,11 +9,11 @@ import css from "../assets/images/css.png";
 import javascript from "../assets/images/javascript.png";
 import php from "../assets/images/php.png";
 import react from "../assets/images/react.png";
-import tailwindCSS from "../assets/images/tailwindcss.png";
 import mySQL from "../assets/images/mysql.png";
 import gitHub from "../assets/images/github.png";
 import VSCode from "../assets/images/visualstudiocode.png";
 import visualStudio from "../assets/images/visualstudio.png";
+import intelliJ from "../assets/images/intellij.png";
 
 
 function Content({page}) {
@@ -25,33 +25,31 @@ function Content({page}) {
                     <img src={me} alt="me"/>
                     <div>
                         <p>
-                            My name is Thanh Quang Doan. I am an international student from Vietnam, currently living in Hamilton
-                            and studying <strong>Software Development</strong> at Mohawk College. My career goal is to begin as a <strong>Front-end Developer</strong> and
-                            gradually expand into <strong>Full-stack Development</strong>, <strong>Mobile App Development</strong>, <strong>Web Application Development</strong>,
-                            or <strong>Game Development</strong>.
+                            My name is Thanh Quang Doan. I am an international student from Vietnam, currently living in Hamilton and
+                            studying <strong>Software Developmen</strong>  at Mohawk College. My career goal is to begin as a Front-end Developer
+                            and gradually expand into a higher level such as <strong>Full-stack Development</strong>, <strong>Mobile App Development</strong>,
+                            and <strong>Game Development</strong>.
                         </p>
                         <p>
-                            Although I do not yet have practical experience in IT or Software Development, I am actively studying in college
-                            and continuously improving my skills through self-learning. In my free time, I also practice problem-solving by
-                            completing coding challenges on <strong>LeetCode</strong>.
+                            I am currently focused on building a strong foundation in software development through formal education and
+                            continuous self-learning. Alongside my college studies, I actively practice problem-solving skills by completing
+                            coding challenges on <strong>LeetCode</strong> and developing personal projects.
                         </p>
                         <p>
-                            Before entering the IT field, I had no programming background. I earned a degree in <strong>English Language</strong> in Vietnam,
-                            specializing in <strong>Translation and Interpretation</strong>, with experience translating notarized documents.
-                            I also worked briefly as <strong>An Assistant to the Director of An International School</strong>. Later, I transitioned into a role
-                            as a <strong>Construction Project Illustrator</strong> at a Japanese company, where I challenged myself in a new field
-                            while applying my self-studied Japanese language skills.
+                            Before transitioning into the IT field, I earned a Bachelor's degree in <strong>English Language</strong> in Vietnam,
+                            specializing in <strong>Translation and Interpretation</strong>. I have professional experience as a Translator,
+                            <strong>an Assistant to the Director of an International School</strong>, and later
+                            as <strong>a Construction Project Illustrator</strong> at a Japanese company.
+                            These roles helped me develop strong adaptability, communication skills, and the ability to learn new domains efficiently.
                         </p>
                         <p>
-                            Now, I am fully committed to pursuing Software Development, driven by a strong interest in technology and programming.
-                            I have been familiar with computers since I was young, so, learning and applying new IT knowledge
-                            has always felt natural and enjoyable for me.
+                            I am now fully committed to pursuing a career in Software Development, driven by a strong interest in technology and programming.
+                            Having been familiar with computers from a very young age, learning and applying new technical knowledge
+                            has always felt natural and motivating to me.
                         </p>
                         <p>
-                            To learn more about me, please have a look at my portfolio, which I built using HTML, CSS, React, and Tailwind CSS.
-                            Both React and Tailwind CSS are technologies that I learned independently without any formal training,
-                            I hope you will kindly understand if there are any imperfections, and I really appreciate if there is any
-                            feedback and opportunities to improve. I am always eager to learn, refine my skills, and grow as a developer.
+                            This portfolio was built using HTML, CSS, and React, which I learned independently through self-study so far.
+                            I am always eager to learn, refine my skills, and grow as a developer through new challenges and opportunities.
                         </p>
                     </div>
                 </section>
@@ -82,10 +80,10 @@ function Content({page}) {
                     </div>
                     <div>
                         <p>
-                            There is a ten-year gap between my academic programs because, after completing my undergraduate degree in my home country,
-                            I entered the workforce to support myself and my family. After a decade of working,
-                            I developed a strong desire to continue my education in a different discipline. My long-standing interest in
-                            technology and information systems led me to pursue further studies abroad, and I chose Mohawk College to begin this transition.
+                            After completing my undergraduate degree, I spent several years working in professional roles
+                            that helped me develop strong adaptability, responsibility, and real-world experience.
+                            Over time, my long-standing interest in technology and information systems led me to pursue
+                            further education in Software Development, which is why I chose Mohawk College to begin this transition.
                         </p>
                     </div>
                 </section>
@@ -96,28 +94,32 @@ function Content({page}) {
                     <h2>Work Experience</h2>
                     <div>
                         <p>
-                            After graduating with a degree in English Language, I worked in various roles that allowed me to develop a diverse skill set.
-                            My first role was as a translator, where I honed my language skills and attention to detail because I mainly worked on paper documents, and it required high accuracy.
-                            I then transitioned to an assistant role at an international school, where I gained organizational and communication skills.
-                            Later, I took on a position as a technical illustrator for construction drawings at a Japanese company.
+                            After graduating with a degree in English Language, I worked in several professional roles that
+                            helped me develop strong attention to detail, communication skills, and adaptability.
+                            These experiences provided a solid foundation thatcontinues to support my transition into software development.
                         </p>
                         <h3>VIET UY TIN CO., LTD.</h3>
                         <ul className="vietuytin-work">
                             <li>Position: Translator</li>
                             <li>Years of work: 2015 - 2019</li>
-                            <li>Responsibilities: Translating notarized office documents between English and Vietnamese with high accuracy.</li>
+                            <li>Translated notarized office documents between English and Vietnamese with high accuracy.</li>
+                            <li>Ensured consistency, confidentiality, and attention to detail in official documentation.</li>
                         </ul>
                         <h3>THE ASIAN INTERNATIONAL SCHOOL</h3>
                         <ul className="asian-school-work">
                             <li>Position: Assistant to the Director</li>
                             <li>Years of work: 2019 - 2021</li>
-                            <li>Responsibilities: Assisting the Director with administrative tasks and communication with parents and staffs.</li>
+                            <li>Assisted the Director with administrative tasks and communication with parents and staff.</li>
+                            <li>Organized documents and supported coordination of school activities.</li>
                         </ul>
                         <h3>YDR VIETNAM CO., LTD. - A branch of Yuasa Design Room Co., Ltd.</h3>
                         <ul className="ydr-work">
                             <li>Position: Technical Illustrator for Construction Drawings</li>
                             <li>Years of work: 2021 - 2024</li>
-                            <li>Responsibilities: Drawing the detailed technical illustrations for construction projects, ensuring accuracy and compliance with industry standards.</li>
+                            <li>
+                                Drew and revised detailed technical illustrations for large-scale construction projects.
+                            </li>
+                            <li>Ensured accuracy and compliance with industry standards through careful review and revisions</li>
                         </ul>
                     </div>
                 </section>
@@ -142,7 +144,6 @@ function Content({page}) {
                                 <img src={javascript} alt="JavaScript"/>
                                 <img src={php} alt="PHP"/>
                                 <img src={react} alt="React"/>
-                                <img src={tailwindCSS} alt="Tailwind CSS"/>
                             </div>
                             <h4>Data Management</h4>
                             <div className="programming-languages">
@@ -153,18 +154,18 @@ function Content({page}) {
                                 <img src={gitHub} alt="GitHub"/>
                                 <img src={VSCode} alt="Visual Studio Code"/>
                                 <img src={visualStudio} alt="Visual Studio"/>
+                                <img src={intelliJ} alt="IntelliJ IDEA"/>
                             </div>
                         </div>
                         <div className="soft-skills">
                             <h3>SOFT SKILLS</h3>
                             <ul className="soft-skills-list">
-                                <li>Effective time management and ability to meet deadlines</li>
-                                <li>Proven ability to work under pressure</li>
-                                <li>Adaptable and quick learner across different fields</li>
-                                <li>Problem-solving with fast response in dynamic situations</li>
-                                <li>High attention to detail and accuracy</li>
-                                <li>Strong work ethic and sense of responsibility</li>
-                                <li>Quick to adapt and communicate effectively within new teams and environments</li>
+                                <li>Effective time management with consistent ability to meet deadlines.</li>
+                                <li>Ability to perform well under pressure in dynamic environments.</li>
+                                <li>Strong problem-solving with a practical and analytical approach.</li>
+                                <li>High attention to detail and accuracy in tasks and documentation.</li>
+                                <li>Strong work ethic and sense of responsibility.</li>
+                                <li>Quick learner with the ability to adapt and communicate effectively within teams.</li>
                             </ul>
                         </div>
                     </div>
